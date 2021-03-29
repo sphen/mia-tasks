58c7c0ad46c9900d71c86146150062dd7bb7904d
sphen <sphen@users.noreply.github.com>
2021-03-29 11:41:56 -0600

    logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <header>
-      <h2 className='site-logo'>Mia Tasks</h2>
+      <Link to='/'>
+        <h2 className='site-logo'>Mia Tasks</h2>
+      </Link>
     </header>
   );
 };
